6037c85cf6f6fa2ab5a8d5d4c54d2ba307dce369
Adam-Elmi <Adamcade123@gmail.com>
2025-10-17 01:01:46 +0300

    Update lexer.js

--- Core/lexer.js
+++ Core/lexer.js
@@ -9,134 +9,113 @@ const file_content = buffer.toString();
 function lexer(raw_source_code) {
   let state = "START";
   const tokens = [];
-  let line = 1,
-    column = 1;
   if (raw_source_code && typeof raw_source_code === "string") {
-    const CONTENT_BLOCKS = raw_source_code.match(
-      /\[\s*[a-zA-Z0-9_]+\s*=?\s*([a-zA-Z0-9_]*\s*\,?)+\s*\]\n([\s\S]*?)\n\[\s*end\s*\]\s*\n*/g,
-    );
-    if (Array.isArray(CONTENT_BLOCKS) && CONTENT_BLOCKS.length > 0) {
-      for (const block of CONTENT_BLOCKS) {
+    const lines = raw_source_code.split(/\n/);
+    if (Array.isArray(lines) && lines.length > 0) {
+      for (let i = 0; i < lines.length; i++) {
         switch (state) {
           case "START":
-            if (/\s*\[\s*/.test(block)) {
+            if (/\s*\n*\[\s*/.test(lines[i])) {
               state = TOKEN_TYPES.OPEN_BRACKET;
               tokens.push({
                 type: TOKEN_TYPES.OPEN_BRACKET,
-                value: block.match(/\s*\[/)[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/\s*\n*\[\s*/)[0],
+                line: i + 1,
+                column: /\s*\n*\[\s*/.exec(lines[i])
+                  ? /\s*\n*\[\s*/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
+            }
+            else if (/\s*[a-zA-Z0-9_]+\s*/.test(lines[i])) {
+              state = TOKEN_TYPES.BLOCK_IDENTIFIER;
+            } else if (/\s*\=+?\s*/.test(lines[i])) {
+              state = TOKEN_TYPES.EQUAL;
+            } else if (/([a-zA-Z0-9_\,\s])+/.test(lines[i])) {
+              state = TOKEN_TYPES.VALUE;
+            } else if (/\s*\]\s*\n?/.test(lines[i])) {
+              state = TOKEN_TYPES.CLOSE_BRACKET;
+            } else if (/([\s\S]*?)/.test(lines[i])) {
+              state = TOKEN_TYPES.CONTENT;
+            } else if (/\s*\[\s*end\s*\]\s*\n*/.test(lines[i])) {
+              state = TOKEN_TYPES.BLOCK_END;
+            } 
+            else {
+              console.error("UNEXPECTED CHARACTER");
             }
           case TOKEN_TYPES.OPEN_BRACKET:
-            if (/\s*[a-zA-Z0-9_]+\s*/.test(block)) {
+            if(/\s*[a-zA-Z0-9_]+\s*(?= \s*\=+?\s*)/.test(lines[i])) {
               state = TOKEN_TYPES.BLOCK_IDENTIFIER;
               tokens.push({
                 type: TOKEN_TYPES.BLOCK_IDENTIFIER,
-                value: block.match(/[a-zA-Z0-9_]+/)[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/\s*[a-zA-Z0-9_]+\s*/)[0],
+                line: i + 1,
+                column: /\s*[a-zA-Z0-9_]+\s*/.exec(lines[i])
+                  ? /\s*[a-zA-Z0-9_]+\s*/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
             }
           case TOKEN_TYPES.BLOCK_IDENTIFIER:
-            if (/\s*\=\s*/.test(block)) {
+            if (/\s*\=+?\s*/.test(lines[i])) {
               state = TOKEN_TYPES.EQUAL;
               tokens.push({
                 type: TOKEN_TYPES.EQUAL,
-                value: block.match(/\s*\=\s*/)[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/\s*\=+?\s*/)[0],
+                line: i + 1,
+                column: /\s*\=+?\s*/.exec(lines[i])
+                  ? /\s*\=+?\s*/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
             }
           case TOKEN_TYPES.EQUAL:
-            if (/\s*([a-zA-Z0-9_]*\s*\,?)+\s*(?=\]\s*\n)/.test(block)) {
+            if (/(?<=\=)\s*(.*?)\s*(?=\s*\]\s*\n*)/.test(lines[i])) {
               state = TOKEN_TYPES.VALUE;
-              const values = block
-                .match(/\s*([a-zA-Z0-9_]*\s*\,?)+\s*(?=\]\s*\n)/)[0]
-                .split(",");
-              if (
-                Array.isArray(values) &&
-                values.length > 0 &&
-                values.length > 1
-              ) {
-                for (const value of values) {
-                  tokens.push({
-                    type: TOKEN_TYPES.VALUE,
-                    value: value,
-                    line: line,
-                    column: column,
-                  });
-                }
-              } else {
-                tokens.push({
-                  type: TOKEN_TYPES.VALUE,
-                  value: block.match(
-                    /\s*([a-zA-Z0-9_]*\s*\,?)+\s*(?=\]\s*\n)/,
-                  )[0],
-                  line: line,
-                  column: column,
-                });
-              }
-            } else {
-              return false;
+              tokens.push({
+                type: TOKEN_TYPES.VALUE,
+                value: lines[i].match(/(?<=\=)\s*(.*?)\s*(?=\s*\]\s*\n*)/)[0],
+                line: i + 1,
+                column: /(?<=\=)\s*(.*?)\s*(?=\s*\]\s*\n*)/.exec(lines[i])
+                  ? /(?<=\=)\s*(.*?)\s*(?=\s*\]\s*\n*)/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
+              });
             }
           case TOKEN_TYPES.VALUE:
-            if (/\s*\]\s*\n/s.test(block)) {
+            if (/\s*\]\s*\n?/.test(lines[i])) {
               state = TOKEN_TYPES.CLOSE_BRACKET;
               tokens.push({
                 type: TOKEN_TYPES.CLOSE_BRACKET,
-                value: block.match(/\s*\]\s*\n/)[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/\s*\]\s*\n?/)[0],
+                line: i + 1,
+                column: /\s*\]\s*\n?/.exec(lines[i])
+                  ? /\s*\]\s*\n?/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
             }
           case TOKEN_TYPES.CLOSE_BRACKET:
-            if (/(?<=\]\n+)([\s\S]*?)(?=\n+\[\s*end\s*\])/s.test(block)) {
+            if (/(.)+?/.test(lines[i])) {
               state = TOKEN_TYPES.CONTENT;
-              const text_block = block.match(
-                /(?<=\]\n+)([\s\S]*?)(?=\n+\[\s*end\s*\])/,
-              )[0];
-              const inline_statements = text_block.match(
-                /[\S]+[^\)]->\([\S]+\)\s*\n?\.?/g,
-              );
-              const inline_statement_with_paren = text_block.match(
-                /\([^)]+\)\s*->\s*\([\S\s]+\)/g,
-              );
-              console.log(inline_statements);
-              console.log(inline_statement_with_paren);
               tokens.push({
                 type: TOKEN_TYPES.CONTENT,
-                value: block.match(
-                  /(?<=\]\n+)([\s\S]*?)(?=\n+\[\s*end\s*\])/,
-                )[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/(.)+/)[0],
+                line: i + 1,
+                column: /(.)+/.exec(lines[i])
+                  ? /(.)+/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
             }
           case TOKEN_TYPES.CONTENT:
-            if (/\s*\[\s*end\s*\]\s*\n*/.test(block)) {
+            if (/\s*\[\s*end\s*\]\s*\n*/.test(lines[i])) {
               state = TOKEN_TYPES.BLOCK_END;
               tokens.push({
                 type: TOKEN_TYPES.BLOCK_END,
-                value: block.match(/\s*\[\s*end\s*\]\s*\n*/)[0],
-                line: line,
-                column: column,
+                value: lines[i].match(/\s*\[\s*end\s*\]\s*\n*/)[0],
+                line: i + 1,
+                column: /\s*\[\s*end\s*\]\s*\n*/.exec(lines[i])
+                  ? /\s*\[\s*end\s*\]\s*\n*/.exec(lines[i]).index + 1
+                  : "UNKNOWN",
               });
-            } else {
-              return false;
             }
         }
-      state = "START";
+        state = "START";
       }
     }
   } else {
