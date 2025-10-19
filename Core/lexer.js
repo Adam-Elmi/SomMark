706886a4d8323905d13fdcb97bb393c809980238
Adam-Elmi <Adamcade123@gmail.com>
2025-10-20 01:28:02 +0300

    Update lexer.js

--- Core/lexer.js
+++ Core/lexer.js
@@ -10,7 +10,7 @@ function lexer(raw_source_code) {
   let state = "START";
   const tokens = [];
   const add_token = (type, lines, index, pattern) => {
-    if (type, lines, index, pattern) {
+    if ((type, lines, index, pattern)) {
       tokens.push({
         type,
         value: lines[index].match(pattern)[0],
@@ -25,13 +25,18 @@ function lexer(raw_source_code) {
       );
     }
   };
-  const open_bracket_pattern = /\s*\[\s*(?=(.)+\s*\=+?\s*(.)+\s*\]\s*\n*?)/;
+  const open_bracket_pattern =
+    /\s*\[\s*(?=(.)+\s*\=+?\s*(.)+\s*\]\s*\n*?|end\s*\])/;
   const block_identifier_pattern = /\s*[a-zA-Z0-9_]+\s*(?= \s*\=+?\s*)/;
   const equal_sign_pattern = /\s*\=+?\s*/;
   const value_pattern = /(?<=\=)\s*(.*?)\s*(?=\s*\]\s*\n*)/;
-  const close_bracket_pattern = /(?<=\s*\[\s*(.)+\s*\=+?\s*(.)+)\s*\]\s*\n?/;
+  const close_bracket_pattern =
+    /(?<=\s*\[\s*(.)+\s*\=+?\s*(.)+|\[\s*end)\s*\]\s*\n?/;
   const content_pattern = /[\s\S]+/;
-  const end_keyword_pattern = /(?<=\s*\[)\s*end\s*(?=\]\s*\n*?)/;
+  const open_at_pattern = /\s*@_(?=\s*(.)+_@)/;
+  const at_identifier_pattern = /(?<=\s*@_)\s*[a-zA-Z0-9_]+[^end]+\s*(?=_@)/;
+  const close_at_pattern = /(?<=\s*@_(.)+)\s*_@\s*\:?\s*/;
+  const end_keyword_pattern = /(?<=(\[|@_))\s*end\s*(?=(\]|_@))/;
   if (raw_source_code && typeof raw_source_code === "string") {
     const lines = raw_source_code.split(/\n/);
     if (Array.isArray(lines) && lines.length > 0) {
@@ -46,6 +51,12 @@ function lexer(raw_source_code) {
                 i,
                 open_bracket_pattern,
               );
+              if (end_keyword_pattern.test(lines[i])) {
+                state = TOKEN_TYPES.END_KEYWORD;
+                console.log("END");
+              }
+            } else if (end_keyword_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.END_KEYWORD;
             } else if (block_identifier_pattern.test(lines[i])) {
               state = TOKEN_TYPES.BLOCK_IDENTIFIER;
             } else if (equal_sign_pattern.test(lines[i])) {
@@ -61,10 +72,20 @@ function lexer(raw_source_code) {
               if (content_pattern.test(lines[i])) {
                 state = TOKEN_TYPES.CONTENT;
               }
-            } else if (end_keyword_pattern.test(lines[i])) {
-              state = TOKEN_TYPES.END_KEYWORD;
+            } else if (open_at_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.OPEN_AT;
+              console.log("OPEN AT");
+            } else if (at_identifier_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.AT_IDENTIFIER;
+              console.log("AT IDENTIFIER");
+              if (!"end".match(at_identifier_pattern)[0]) {
+                state = TOKEN_TYPES.END_KEYWORD;
+              }
+            } else if (close_at_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.CLOSE_AT;
+              console.log("CLOSE AT");
             } else {
-              console.error("UNEXPECTED CHARACTER");
+              console.error(`UNEXPECTED CHARACTER AT LINE ${lines[i]}`);
             }
           case TOKEN_TYPES.OPEN_BRACKET:
             if (block_identifier_pattern.test(lines[i])) {
@@ -106,7 +127,36 @@ function lexer(raw_source_code) {
                 add_token(TOKEN_TYPES.CONTENT, lines, i, content_pattern);
               }
             }
+
           case TOKEN_TYPES.CONTENT:
+            if (open_at_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.OPEN_AT;
+              add_token(TOKEN_TYPES.OPEN_AT, lines, i, open_at_pattern);
+            }
+
+          case TOKEN_TYPES.OPEN_AT:
+            if (at_identifier_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.AT_IDENTIFIER;
+              add_token(
+                TOKEN_TYPES.AT_IDENTIFIER,
+                lines,
+                i,
+                at_identifier_pattern,
+              );
+            }
+
+          case TOKEN_TYPES.AT_IDENTIFIER:
+            if (close_at_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.CLOSE_AT;
+              add_token(TOKEN_TYPES.CLOSE_AT, lines, i, close_at_pattern);
+            }
+
+          case TOKEN_TYPES.CLOSE_AT:
+            if (end_keyword_pattern.test(lines[i])) {
+              state = TOKEN_TYPES.END_KEYWORD;
+            }
+
+          case TOKEN_TYPES.END_KEYWORD:
             if (end_keyword_pattern.test(lines[i])) {
               state = TOKEN_TYPES.END_KEYWORD;
               add_token(TOKEN_TYPES.END_KEYWORD, lines, i, end_keyword_pattern);
