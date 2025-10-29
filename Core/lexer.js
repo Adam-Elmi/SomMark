001b5e2cb7e782b82f4068d2b9463cf048d69ca7
Adam-Elmi <Adamcade123@gmail.com>
2025-10-29 03:22:34 +0300

    Update lexer.js

--- Core/lexer.js
+++ Core/lexer.js
@@ -15,55 +15,15 @@ function peek(input, index, offset) {
   return null;
 }
 
-function detector(input, index, expected_char = []) {
-  let is_special_token = false;
-  for (let ch = index; ch < input.length; ch++) {
-    let char = input[ch];
-    if (expected_char.includes(char)) {
-      is_special_token = true;
-      break;
-    }
-  }
-  return is_special_token;
-}
-
-function advance(input, index, current_character) {
-  let _is = false;
-  switch (current_character) {
-    case "[":
-      _is = detector(
-        input,
-        index,
-         ["=", "]"],
-      );
-      return _is;
-    case "=":
-      _is = detector(
-        input,
-        index,
-        ["]"],
-      );
-      return _is;
-    case "]":
-      _is = detector(
-        input,
-        index,
-         ["\n"],
-      );
-      return _is;
-  }
-}
-
-function concat_char(input, index, advance, stop_at_char = []) {
+function concat_char(input, index, mode = "normal", stop_at_char = []) {
   let str = "";
   for (let char_index = index; char_index < input.length; char_index++) {
     let char = input[char_index];
-    if (typeof advance === "function") {
-      if (advance(input, index, char)) {
-        break;
-      } else {
+    if (mode === "normal") {
+     if(char === "\n") {
+       break;
+     }
         str += char;
-      }
     } else {
       if (char === "=" || char === "]") {
         prev_special_token = char;
@@ -88,33 +48,39 @@ function lexer(src) {
 
     for (let i = 0; i < src.length; i++) {
       let current_char = src[i];
-      if (current_char === "[" && advance(src, i, current_char)) {
+      console.log("Prev token: ", BLOCK_STACK)
+      if (current_char === "[" && BLOCK_STACK.length === 0) {
         add_token(TOKEN_TYPES.OPEN_BRACKET, current_char);
         BLOCK_STACK.push(current_char);
         prev_special_token = current_char;
-      } else if (current_char === "=" && advance(src, i, current_char)) {
-        BLOCK_STACK.push(current_char);
+      } else if (current_char === "=" && BLOCK_STACK.includes("[")) {
         add_token(TOKEN_TYPES.EQUAL, current_char);
-      } else if (current_char === "]" && advance(src, i, current_char)) {
+        BLOCK_STACK.push(current_char);
+      } else if (current_char === "]" && BLOCK_STACK.includes("[")) {
         add_token(TOKEN_TYPES.CLOSE_BRACKET, current_char);
+        BLOCK_STACK = [];
       } else if (current_char === "\n") {
         add_token(TOKEN_TYPES.NEWLINE, current_char);
       } else {
         if (prev_special_token === "[") {
-          temp_str = concat_char(src, i, null, ["=", "]"]);
+          temp_str = concat_char(src, i, "active", ["=", "]"]);
           if (temp_str) {
             i += temp_str.length - 1;
           }
-          add_token(TOKEN_TYPES.BLOCK_IDENTIFIER, temp_str);
+          if(temp_str.trim() === "end") {
+            add_token(TOKEN_TYPES.END_KEYWORD, temp_str);
+          } else {
+            add_token(TOKEN_TYPES.BLOCK_IDENTIFIER, temp_str);
+          }
         } else if (prev_special_token === "=") {
-          temp_str = concat_char(src, i, null, ["]"]);
+          temp_str = concat_char(src, i, "active", ["]"]);
           if (temp_str) {
             i += temp_str.length - 1;
             current_char = src[i];
           }
           add_token(TOKEN_TYPES.VALUE, temp_str);
         } else {
-          context = concat_char(src, i, advance);
+          context = concat_char(src, i);
           if (context) {
             i += context.length - 1;
             current_char = src[i];
