6d525b7917b76addb0a4d64a45f38e83327a1fab
Adam-Elmi <Adamcade123@gmail.com>
2025-11-16 19:48:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import SomMark from "./Core/sommark.js";
 
 const buffer = await fs.readFile("./tests/examples/simple.smark");
 const file_content = buffer.toString();
-let smark = new SomMark;
+let smark = new SomMark(file_content);
 
-console.log(smark.lex(file_content));
+console.log(smark.lex());
+// smark.parse()
