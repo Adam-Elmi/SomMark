4ef5c98555664f6938799988f1743797f2159cdd
Adam-Elmi <Adamcade123@gmail.com>
2025-11-16 22:25:14 +0300

    Update parser.js

--- Core/parser.js
+++ Core/parser.js
@@ -1,3 +1,4 @@
+import { text } from "node:stream/consumers";
 import peek from "./helpers/peek.js";
 import TOKEN_TYPES from "./tokenTypes.js";
 
@@ -49,76 +50,169 @@ function makeAtBlockNode() {
 function parseBlock(tokens, i) {
   const blockNode = makeBlockNode();
   if (current_token(tokens, i).value !== "[") {
-    throw new Error("Expected '['");
+    throw new Error("Expected token '['");
   }
   i++;
 
   if (current_token(tokens, i).type === TOKEN_TYPES.IDENTIFIER) {
-    blockNode.id = current_token(tokens, i).value.trim();
+    blockNode.id = current_token(tokens, i).value;
   } else {
-    if(current_token(tokens, i).value === "=") {
-      throw new Error("Expected block identifier before '='");
+    if (current_token(tokens, i).value === "=") {
+      throw new Error("Expected token 'block identifier' before '='");
     } else {
-      throw new Error("Expected '[' before block identifier");
+      throw new Error("Expected token '[' before block identifier");
     }
   }
   i++;
-  if ( current_token(tokens, i) && current_token(tokens, i).value === "=") {
+  if (current_token(tokens, i) && current_token(tokens, i).value === "=") {
     i++;
     if (current_token(tokens, i).type === TOKEN_TYPES.VALUE) {
-      current_token(tokens, i).value.trim().split(",").forEach((value) => {
-        blockNode.args.push(value);
-      })
+      current_token(tokens, i)
+        .value.split(",")
+        .forEach((value) => {
+          blockNode.args.push(value);
+        });
     } else {
-      throw new Error("Expected block value after '='");
+      throw new Error("Expected token 'block value' after '='");
     }
     i++;
   }
 
   if (current_token(tokens, i) && current_token(tokens, i).value !== "]") {
-    throw new Error("Expected ']'");
+    if (current_token(tokens, i).value === "=") {
+      throw new Error("Expected token ']' after block value");
+    } else {
+      throw new Error("Expected token ']' after block identifier");
+    }
   }
   i++;
-  
-  while(i < tokens.length) {
-    if(current_token(tokens, i).value === "[" && peek(tokens, i, 1).value !== "end") {
+
+  while (i < tokens.length) {
+    if (
+      current_token(tokens, i).value === "[" &&
+      peek(tokens, i, 1).value !== "end"
+    ) {
       const [childNode, nextIndex] = parseBlock(tokens, i);
       blockNode.body.push(childNode);
       i = nextIndex;
-    } 
-    else if(current_token(tokens, i).value === "[" && peek(tokens, i, 1).value === "end") {
-      console.log(peek(tokens, i, 2));
-      if(peek(tokens, i, 2) === null || peek(tokens, i, 2).value !== "]") {
-        throw new Error("Expected ']' after 'end'");
+    } else if (
+      current_token(tokens, i).value === "[" &&
+      peek(tokens, i, 1).value === "end"
+    ) {
+      if (peek(tokens, i, 2) === null || peek(tokens, i, 2).value !== "]") {
+        throw new Error("Expected token ']' after 'end'");
       }
       i += 2;
       break;
     } else {
-      i++;
+      let [childNode, nextIndex] = parseNode(tokens, i);
+      if (!childNode) {
+        i += 1;
+        continue;
+      }
+      blockNode.body.push(childNode);
+      i = nextIndex;
     }
   }
   return [blockNode, i];
 }
 
-function parseText() {}
+function parseInline(tokens, i) {
+  const inlineNode = makeInlineNode();
+  if (!current_token(tokens, i) || current_token(tokens, i).value !== "(") {
+    throw new error("Expected token '('");
+  }
+  i++;
+  if (current_token(tokens, i).type === TOKEN_TYPES.VALUE) {
+    inlineNode.value = current_token(tokens, i).value;
+  } else {
+    throw new Error("Expected token 'inline value'");
+  }
+  i++;
+  if (!current_token(tokens, i) || current_token(tokens, i).value !== ")") {
+    throw new error("Expected token ')'");
+  }
+  i++;
+  if (!current_token(tokens, i) || current_token(tokens, i).value !== "->") {
+    throw new error("Expected token '->'");
+  }
+  i++;
+  if (!current_token(tokens, i) || current_token(tokens, i).value !== "(") {
+    throw new error("Expected token '('");
+  }
+  i++;
+  if (
+    current_token(tokens, i) &&
+    current_token(tokens, i).type === TOKEN_TYPES.IDENTIFIER
+  ) {
+    inlineNode.id = current_token(tokens, i).value;
+  } else {
+    throw new Error("Expected token 'inline identifier'");
+  }
+  i++;
+  if (!current_token(tokens, i) || current_token(tokens, i).value !== ")") {
+    throw new error("Expected token ')'");
+  }
+  i++;
+  return [inlineNode, i];
+}
 
-function parseInline() {}
+function parseText(tokens, i) {
+  const textNode = makeTextNode();
+  if (
+    current_token(tokens, i) &&
+    current_token(tokens, i).type === TOKEN_TYPES.CONTENT
+  ) {
+    textNode.text = current_token(tokens, i).value;
+  }
+  i++;
+  while (i < tokens.length) {
+    if (current_token(tokens, i) && current_token(tokens, i).value === "(") {
+      const [node, nextIndex] = parseInline(tokens, i);
+      textNode.text += " " + node.value;
+      textNode.inline.push(node);
+      i = nextIndex;
+    } else if (
+      current_token(tokens, i) &&
+      current_token(tokens, i).type === TOKEN_TYPES.CONTENT
+    ) {
+      const [node, nextIndex] = parseText(tokens, i);
+      textNode.text += " " + node.text;
+      node.inline.forEach((value) => {
+        textNode.inline.push(value);
+      });
+      i = nextIndex;
+    } else {
+      break;
+    }
+  }
+  return [textNode, i];
+}
 
 function parseAtBlock() {}
 
 function parseNode(tokens, i) {
   if (current_token(tokens, i).value === "[" && peek(tokens, i, 1) !== "end") {
     return parseBlock(tokens, i);
-  }  {
+  } else if (
+    current_token(tokens, i) &&
+    current_token(tokens, i).type === TOKEN_TYPES.CONTENT
+  ) {
+    return parseText(tokens, i);
+  } else if (
+    current_token(tokens, i) &&
+    current_token(tokens, i).value === "("
+  ) {
+    return parseInline(tokens, i);
   }
-   return [null, i + 1];
+  return [null, i + 1];
 }
 
 function parser(tokens) {
   const ast = [];
   for (let i = 0; i < tokens.length; i++) {
     let [nodes, nextIndex] = parseNode(tokens, i);
-    if(nodes) {
+    if (nodes) {
       ast.push(nodes);
       i = nextIndex;
     } else {
