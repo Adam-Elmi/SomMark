f491cdf3c229fe13f7ccd2aaa6bc14c07052f3c2
Adam-Elmi <Adamcade123@gmail.com>
2025-11-21 18:07:44 +0300

    Add: initial implement...

--- js/core/transpiler.js
+++ js/core/transpiler.js
@@ -1,6 +1,60 @@
 import parser from "./parser.js";
 import peek from "../helpers/peek.js";
+import html_mapping from "../mapping/default_mode/smark.html.js";
+import markdown_mapping from "../mapping/default_mode/smark.md.js";
+import fs from "node:fs/promises";
+import path from "path";
 
-function transpiler() {
-  
+const file_path = path.join(process.cwd(), "smark.config.js");
+const isFileExist = await fs
+  .access(file_path)
+  .then(() => true)
+  .catch(() => false);
+const config_file = isFileExist ? await import(file_path) : null;
+// console.log(config_file.default);
+
+function transpileToHtml(ast, i) {
+  const node = Array.isArray(ast) ? ast[i] : ast;
+  let result = "";
+  if (node.type === "Block") {
+    let target = html_mapping.outputs.find((value) => value.id === node.id);
+    if (target) {
+      result = target.output(node.args, "<%smark>");
+      let context = "";
+      for (const n of node.body) {
+        switch (n.type) {
+          case "Text":
+            context += n.text;
+            break;
+          case "Inline":
+            context += n.value;
+            break;
+          case "Newline":
+            context += n.value;
+            break;
+          case "Block":
+            target = html_mapping.outputs.find((value) => value.id === n.id);
+            context += transpileToHtml(n, i);
+            break;
+        }
+      }
+      result = result.replace("<%smark>", context);
+    } else {
+      console.log("Unknown blocks:", node.id);
+    }
+  }
+  return result;
 }
+
+function transpiler(ast) {
+  let output = "";
+  console.log(ast);
+  for (let i = 0; i < ast.length; i++) {
+    if (ast[i].type === "Block") {
+      output += transpileToHtml(ast, i);
+    }
+  }
+  return output;
+}
+
+export default transpiler;
