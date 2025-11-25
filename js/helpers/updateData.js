eb54bd59e789b48e62f549a1dd16ecdfb52e8dc4
Adam-Elmi <Adamcade123@gmail.com>
2025-11-25 06:43:06 +0300

    Update updateData.js

--- js/helpers/updateData.js
+++ js/helpers/updateData.js
@@ -1,35 +1,24 @@
 function updateStack(stack, t, stackValue, t_val) {
-  stack.push(stackValue);
-  t.push(t_val);
+	stack.push(stackValue);
+	t.push(t_val);
 }
 
-function updateColumn(input, index, text, col, are_same = true) {
-  let start, end;
-  if (are_same) {
-    start = index + 1;
-    return {
-      start,
-      end: start,
-    };
-  } else {
-    start = index + 1;
-    index += text.length - 1;
-    end = index + 1;
-    return [
-      index,
-      {
-        start,
-        end,
-      },
-    ];
-  }
+
+function updateProps(target, data1, data2, index, type) {
+	target = data1[index];
+	target.type = type[index];
+	target.value = data2[index];
+	return target;
 }
 
-function updateProps(target, data1, data2 ,index, type) {
-  target = data1[index];
-  target.type = type[index];
-  target.value = data2[index];
-  return target;
+function resetArray(...arrays) {
+	for (const arr of arrays) {
+		if (Array.isArray(arr)) {
+			arr.length = 0;
+		} else {
+			throw new Error(`${arr} is not array`);
+		}
+	}
 }
 
-export { updateStack, updateColumn, updateProps };
+export { updateStack, updateProps, resetArray };
