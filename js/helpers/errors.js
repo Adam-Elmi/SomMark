a44b6eb8a418a0b335b21a61ed5f121ba533d415
Adam-Elmi <Adamcade123@gmail.com>
2025-11-28 00:24:15 +0300

    Update errors.js

--- js/helpers/errors.js
+++ js/helpers/errors.js
@@ -4,13 +4,33 @@ class ParserError extends Error {
 	constructor(message, line, start, end, token = "Unknown") {
 		super(message);
     this.name = "ParserError";
-    this.message = `${colorize("cyan", this.name)}:\n-------------------------------------------------------\n${colorize("red", message)} at line ${colorize("yellow", line)}, from column ${colorize("yellow", start)} to ${colorize("yellow", end)}\n-------------------------------------------------------\n${colorize("blue", "Token we got")}: ${colorize("magenta", token === "\n" ? "\\n" : "'" +token + "'")} at line ${colorize("yellow", line)}, from column ${colorize("yellow", start)} to ${colorize("yellow", end)}\n-------------------------------------------------------\n
-		`;
+    const formatMessage = [
+      colorize("cyan", this.name),
+      "\n-------------------------------------------------------\n",
+      colorize("red", message),
+      
+      " at line ",
+      colorize("yellow", line),
+      ", from column ",
+      colorize("yellow", start),
+      " to ",
+      colorize("yellow", end),
+      "\n-------------------------------------------------------\n",
+      colorize("blue", "Token we got: "),
+      colorize("magenta", token === "\n" ? "\\n" : "'" +token + "'"),
+      " at line ",
+      colorize("yellow", line),
+      ", from column ",
+      colorize("yellow", start),
+      " to ",
+      colorize("yellow", end),
+      "\n-------------------------------------------------------\n"
+    ];
+    this.message = formatMessage.join("");
 		this.line = line;
 		this.start = start;
 		this.end = end;
 		this.token = token;
 	}
 }
-
 export { ParserError };
