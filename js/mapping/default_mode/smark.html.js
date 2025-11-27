22487ad2ed2141092e0fef13dc0158679495a3c1
Adam-Elmi <Adamcade123@gmail.com>
2025-11-28 02:59:39 +0300

    Update smark.html.js

--- js/mapping/default_mode/smark.html.js
+++ js/mapping/default_mode/smark.html.js
@@ -1,13 +1,13 @@
 import Mapping from "../mapping.js";
 
 const html_mapping = new Mapping();
-
-html_mapping.create("Block1", "block", (args, body) => {
-  return `<p style='color:${args[0]};'>${body}</p>`;
+const { tag } = html_mapping;
+html_mapping.create("Section", (args, children) => {
+	return tag("p").attributes({ class: args[0] }).body(children).render();
 });
 
-html_mapping.create("Block2", "block", (args, body) => {
-  return `\n<a href='${args[0]}'>${body}\n</a>\n`;
+html_mapping.create("code", (args, children) => {
+	return tag("code").attributes({ class: args[0] }).body(children).render();
 });
 
 export default html_mapping;
