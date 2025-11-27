9bcb95d78cd4a63ccc69ace5f3b5b3a4aafb33ea
Adam-Elmi <Adamcade123@gmail.com>
2025-11-28 02:59:43 +0300

    Update mapping.js

--- js/mapping/mapping.js
+++ js/mapping/mapping.js
@@ -1,30 +1,26 @@
+import TagBuilder from "../formatter/tag.js";
 class Mapping {
-  constructor() {
-    this.outputs = [];
-  }
-  create(id, type, output) {
-    if (id && type && output) {
-      if (typeof id !== "string") {
-        throw new Error("argument 'id' expected to be a string");
-      }
-      if (typeof type !== "string") {
-        throw new Error("argument 'type' expected to be a string");
-      }
-      if (typeof output !== "function") {
-        throw new Error("argument 'output' expected to be a function");
-      }
-      if(type !== "block" && type !== "inline" && type !== "atblock") {
-        throw new Error("Invalid type: expected type: 'block' | 'inline' | 'atblock'");
-      }
-      this.outputs.push({
-        id,
-        type,
-        output,
-      });
-    } else {
-      throw new Error("Expected arguments are not defined");
-    }
-  }
+	constructor() {
+		this.outputs = [];
+	}
+	create(id, output) {
+		if (id && output) {
+			if (typeof id !== "string") {
+				throw new Error("argument 'id' expected to be a string");
+			}
+			if (typeof output !== "function") {
+				throw new Error("argument 'output' expected to be a function");
+			}
+			this.outputs.push({
+				id,
+				output
+			});
+		} else {
+			throw new Error("Expected arguments are not defined");
+		}
+	}
+	tag(tagName) {
+		return new TagBuilder(tagName);
+	}
 }
-
 export default Mapping;
