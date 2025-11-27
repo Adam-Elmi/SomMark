2c4024beebfe6063725f9480e8905e8582199775
Adam-Elmi <Adamcade123@gmail.com>
2025-11-27 07:09:12 +0300

    Update concat.js

--- js/helpers/concat.js
+++ js/helpers/concat.js
@@ -1,28 +1,43 @@
 import peek from "./peek.js";
-function concat(input, index, exclude_stop_char = true, stop_at_char = [], scope_state) {
-  let str = "";
-  for (let char_index = index; char_index < input.length; char_index++) {
-    let char = input[char_index];
-    if (exclude_stop_char) {
-      if (char === "\n") {
-        break;
-      } else if (char === "[" && scope_state === false) {
-        break;
-      } else if (char === "(" && scope_state === false) {
-        break;
-      } else if (char === "@" && peek(input, char_index, 1) === "_" && scope_state === false) {
-        break;
-      }
-      str += char;
-    } else {
-      if (stop_at_char.includes(char)) {
-        break;
-      }
-      str += char;
-    }
-  }
+function concat(input, index, exclude_stop_char = true, stop_at_char = [], scope_state, include_then_break = false) {
+	let str = "";
+	for (let char_index = index; char_index < input.length; char_index++) {
+		let char = input[char_index];
+		if (exclude_stop_char) {
+			if (char === "\n") {
+				break;
+			} else if (char === "[" && scope_state === false) {
+				break;
+			} else if (char === "=" && scope_state === false) {
+				break;
+			} else if (char === "]" && scope_state === false) {
+				break;
+			} else if (char === "(" && scope_state === false) {
+				break;
+			} else if (char === "-" && peek(input, char_index, 1) === ">" && scope_state === false) {
+				break;
+			} else if (char === "@" && peek(input, char_index, 1) === "_" && scope_state === false) {
+				break;
+			} else if (char === "_" && peek(input, char_index, 1) === "@" && scope_state === false) {
+				break;
+			} else if (char === "#" && scope_state === false) {
+				break;
+			} else if (char === "`" && scope_state === false) {
+				break;
+			}
+			str += char;
+		} else {
+			if (char_index === input.length - 1) {
+				include_then_break ? (str += char) : null;
+			}
+			if (stop_at_char.includes(char)) {
+				break;
+			}
+			str += char;
+		}
+	}
 
-  return str;
+	return str;
 }
 
 export default concat;
