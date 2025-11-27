48320a19e667a09b2988c56208015d2b796b90f3
Adam-Elmi <Adamcade123@gmail.com>
2025-11-27 08:17:58 +0300

    Update smark.html.js

--- js/mapping/default_mode/smark.html.js
+++ js/mapping/default_mode/smark.html.js
@@ -1,9 +1,9 @@
-import Mapping from "../../formatter/mapping.js";
+import Mapping from "../mapping.js";
 
 const html_mapping = new Mapping();
 
 html_mapping.create("Block1", "block", (args, body) => {
-  return `<p style='color:${args[0]};'>${body}\n</p>`;
+  return `<p style='color:${args[0]};'>${body}</p>`;
 });
 
 html_mapping.create("Block2", "block", (args, body) => {
