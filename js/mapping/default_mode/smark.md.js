2da5f7dcc10dee38ae443a174d95bf138ad00406
Adam-Elmi <Adamcade123@gmail.com>
2025-11-29 02:01:56 +0300

    Update smark.md.js

--- js/mapping/default_mode/smark.md.js
+++ js/mapping/default_mode/smark.md.js
@@ -1,8 +1,7 @@
 import Mapping from "../mapping.js";
-
-const markdown_mapping = new Mapping();
-const { md } = markdown_mapping;
-markdown_mapping.create("Heading", (args, children) => {
+const markdown = new Mapping();
+const { md } = markdown;
+markdown.create("Heading", (args, children) => {
 	return md.heading(children, args[0]);
 });
-export default markdown_mapping;
+export default markdown;
