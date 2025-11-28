ddef8895bdc7cb92f7397d61ac7903ef83eedc53
Adam-Elmi <Adamcade123@gmail.com>
2025-11-28 23:00:05 +0300

    Update smark.html.js

--- js/mapping/default_mode/smark.html.js
+++ js/mapping/default_mode/smark.html.js
@@ -1,11 +1,7 @@
 import Mapping from "../mapping.js";
-
 const html_mapping = new Mapping();
 const { tag } = html_mapping;
 html_mapping.create("Section", (args, children) => {
-	return tag("p").attributes({ class: args[0] }).body(children).render();
-});
-html_mapping.create(["Bold", "bold", "b", "B"], (args, children) => {
-	return tag("strong").body(children).render();
+	return tag("section").body(children).render();
 });
 export default html_mapping;
