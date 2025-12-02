bec98140ccb5543d3b7fc380542656623925a01f
Adam-Elmi <Adamcade123@gmail.com>
2025-12-02 07:13:33 +0300

    Update sommark.js

--- js/core/sommark.js
+++ js/core/sommark.js
@@ -1,9 +1,12 @@
 import lexer from "./lexer.js";
 import parser from "./parser.js";
+import transpiler from "./transpiler.js";
+import html from "../mapping/default_mode/smark.html.js";
 
 class SomMark {
-  constructor(src) {
+  constructor(src, format) {
     this.src = src;
+    this.format = format;
   }
   lex() {
     return lexer(this.src);
@@ -11,6 +14,10 @@ class SomMark {
   parse() {
     const tokens = this.lex();
     return parser(tokens);
+  }
+  transpile() {
+    const ast = this.parse();
+    return transpiler(ast, this.format, html);
   }
 }
 
