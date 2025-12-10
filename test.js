b513c422bbbf493fb1f086a2b6b41a83c7318e14
Adam-Elmi <Adamcade123@gmail.com>
2025-12-10 15:02:44 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 import fs from "node:fs/promises";
 import SomMark from "./js/core/sommark.js";
+import markdown from "./js/mapping/default_mode/smark.md.js";
+import html from "./js/mapping/default_mode/smark.html.js";
 
 const buffer = await fs.readFile("./tests/examples/basic.smark");
 const file_content = buffer.toString();
-let smark = new SomMark(file_content);
+let smark = new SomMark(file_content, "md", markdown);
 
-console.log(JSON.stringify(smark.parse(), null, 2));
+// console.log(JSON.stringify(smark.parse(), null, 2));
+// console.log(smark.lex());
+console.log(smark.transpile());
