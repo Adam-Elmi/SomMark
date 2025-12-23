be7b523d862b7831a42980cc5492f3cf05ba1b50
Adam-Elmi <Adamcade123@gmail.com>
2025-12-23 14:32:17 +0300

    Rename dir "mapping" to "mappers"

---
+++ mappers/default_mode/smark.html.js
@@ -0,0 +1,69 @@
+import Mapping from "../mapper.js";
+const html = new Mapping();
+const { tag, setHeader, highlightCode } = html;
+
+// Block
+html.create("Block", (_, children) => {
+	return children;
+});
+// Section
+html.create("Section", (_, children) => {
+	return tag("section").body(children);
+});
+// Headings
+html.create("h1", (_, children) => {
+	return tag("h1").body(children);
+});
+html.create("h2", (_, children) => {
+	return tag("h2").body(children);
+});
+html.create("h3", (_, children) => {
+	return tag("h3").body(children);
+});
+html.create("h4", (_, children) => {
+	return tag("h4").body(children);
+});
+html.create("h5", (_, children) => {
+	return tag("h5").body(children);
+});
+html.create("h6", (_, children) => {
+	return tag("h6").body(children);
+});
+// Bold
+html.create(["bold", "b"], (_, children) => {
+	return tag("strong").body(children);
+});
+// Italic
+html.create(["italic", "i"], (_, children) => {
+	return tag("i").body(children);
+});
+// Italic
+html.create(["emphasis", "e"], (_, children) => {
+	return tag("span").attributes({ style: "font-weight:bold; font-style: italic;" }).body(children);
+});
+// Colored Text
+html.create("color", (args, children) => {
+	return tag("span")
+		.attributes({ style: `color:${args[0]}` })
+		.body(children);
+});
+// Link
+html.create("link", (args, children) => {
+	return tag("a").attributes({ href: args[0], title: args[1] }).body(children);
+});
+// Image
+html.create("image", (args, children) => {
+	return tag("img").selfClose().attributes({ src: args[0], alt: children }).body("");
+});
+// Code
+html.create("code", (args, children) => {
+	const [content, cssTheme] = highlightCode(children, args[0].trim());
+	setHeader(null, { rawData: [cssTheme] });
+	return tag("pre").body(
+		tag("code")
+			.attributes({ class: `hljs language-${args[0].trim()}` })
+			.body(content)
+	);
+});
+
+export default html;
