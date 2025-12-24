a83720e31984ac210c4dd043d9ff1f20c7a331b9
Adam-Elmi <Adamcade123@gmail.com>
2025-12-24 03:12:41 +0300

    Update smark.md.js

--- mappers/default_mode/smark.md.js
+++ mappers/default_mode/smark.md.js
@@ -1,67 +1,68 @@
-import Mapping from "../mapper.js";
-const markdown = new Mapping();
+import Mapper from "../mapper.js";
+const markdown = new Mapper();
 const { md } = markdown;
 // Block
-markdown.create("Block", (_, children) => {
-  return children;
+markdown.create("Block", ({args, content}) => {
+  return content;
 });
 // Headings
-markdown.create("Heading", args => {
+markdown.create("Heading", ({args, content}) => {
+  console.log(args, content)
 	return md.heading(args[1], args[0]);
 });
 // Inline Headings
-markdown.create("h1", (_, children) => {
-	return md.heading(children, 1);
+markdown.create("h1", ({args, content}) => {
+	return md.heading(content, 1);
 });
-markdown.create("h2", (_, children) => {
-	return md.heading(children, 2);
+markdown.create("h2", ({args, content}) => {
+	return md.heading(content, 2);
 });
-markdown.create("h3", (_, children) => {
-	return md.heading(children, 3);
+markdown.create("h3", ({args, content}) => {
+	return md.heading(content, 3);
 });
-markdown.create("h4", (_, children) => {
-	return md.heading(children, 4);
+markdown.create("h4", ({args, content}) => {
+	return md.heading(content, 4);
 });
-markdown.create("h5", (_, children) => {
-	return md.heading(children, 5);
+markdown.create("h5", ({args, content}) => {
+	return md.heading(content, 5);
 });
-markdown.create("h6", (_, children) => {
-	return md.heading(children, 6);
+markdown.create("h6", ({args, content}) => {
+	return md.heading(content, 6);
 });
 // Bold
-markdown.create(["bold", "b"], (_, children) => {
-	return md.bold(children);
+markdown.create(["bold", "b"], ({args, content}) => {
+	return md.bold(content);
 });
 // Italic
-markdown.create(["italic", "i"], (_, children) => {
-	return md.italic(children);
+markdown.create(["italic", "i"], ({args, content}) => {
+	return md.italic(content);
 });
 // Bold and Italic (emphasis)
-markdown.create(["emphasis", "e"], (_, children) => {
-	return md.emphasis(children);
+markdown.create(["emphasis", "e"], ({args, content}) => {
+	return md.emphasis(content);
 });
 // Code Blocks
-markdown.create(["code", "codeBlock"], (args, children) => {
-	return md.codeBlock(children, args[0]);
+markdown.create(["code", "codeBlock"], ({args, content}) => {
+	return md.codeBlock(content, args[0]);
 });
 // Link
-markdown.create("link", (args, children) => {
-	return md.url("link", children, args[0], args[1]);
+markdown.create("link", ({args, content}) => {
+	return md.url("link", content, args[0], args[1]);
 });
 // Image
-markdown.create("image", (args, children) => {
-	return md.url("image", children, args[0], args[1]);
+markdown.create("image", ({args, content}) => {
+	return md.url("image", content, args[0], args[1]);
 });
 // Horizontal Rule
-markdown.create(["horizontal", "h"], (_, children) => {
-	return md.horizontal(children);
+markdown.create(["horizontal", "h"], ({args, content}) => {
+	return md.horizontal(content);
 });
 // Escape Characters
-markdown.create(["escape", "s"], (_, children) => {
-	return md.escape(children);
+markdown.create(["escape", "s"], ({args, content}) => {
+	return md.escape(content);
 });
 // Table
-markdown.create("table", (args, children) => {
-	return md.table(args, children.split("\n"));
+markdown.create("table", ({args, content}) => {
+	return md.table(args, content.split("\n"));
 });
 export default markdown;
