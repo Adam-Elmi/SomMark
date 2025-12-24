555a59782ab076a7a73e8f263de4310b015d32d4
Adam-Elmi <Adamcade123@gmail.com>
2025-12-24 03:12:37 +0300

    Update smark.html.js

--- mappers/default_mode/smark.html.js
+++ mappers/default_mode/smark.html.js
@@ -1,69 +1,69 @@
-import Mapping from "../mapper.js";
-const html = new Mapping();
-const { tag, setHeader, highlightCode } = html;
+import Mapper from "../mapper.js";
+const html = new Mapper();
+const { tag, setHeader, highlightCode, cssTheme } = html;
 
 // Block
-html.create("Block", (_, children) => {
-	return children;
+html.create("Block", ({ args, content }) => {
+	return content;
 });
 // Section
-html.create("Section", (_, children) => {
-	return tag("section").body(children);
+html.create("Section", ({ args, content }) => {
+	return tag("section").body(content);
 });
 // Headings
-html.create("h1", (_, children) => {
-	return tag("h1").body(children);
+html.create("h1", ({ args, content }) => {
+	return tag("h1").body(content);
 });
-html.create("h2", (_, children) => {
-	return tag("h2").body(children);
+html.create("h2", ({ args, content }) => {
+	return tag("h2").body(content);
 });
-html.create("h3", (_, children) => {
-	return tag("h3").body(children);
+html.create("h3", ({ args, content }) => {
+	return tag("h3").body(content);
 });
-html.create("h4", (_, children) => {
-	return tag("h4").body(children);
+html.create("h4", ({ args, content }) => {
+	return tag("h4").body(content);
 });
-html.create("h5", (_, children) => {
-	return tag("h5").body(children);
+html.create("h5", ({ args, content }) => {
+	return tag("h5").body(content);
 });
-html.create("h6", (_, children) => {
-	return tag("h6").body(children);
+html.create("h6", ({ args, content }) => {
+	return tag("h6").body(content);
 });
 // Bold
-html.create(["bold", "b"], (_, children) => {
-	return tag("strong").body(children);
+html.create(["bold", "b"], ({ args, content }) => {
+	return tag("strong").body(content);
 });
 // Italic
-html.create(["italic", "i"], (_, children) => {
-	return tag("i").body(children);
+html.create(["italic", "i"], ({ args, content }) => {
+	return tag("i").body(content);
 });
 // Italic
-html.create(["emphasis", "e"], (_, children) => {
-	return tag("span").attributes({ style: "font-weight:bold; font-style: italic;" }).body(children);
+html.create(["emphasis", "e"], ({ args, content }) => {
+	return tag("span").attributes({ style: "font-weight:bold; font-style: italic;" }).body(content);
 });
 // Colored Text
-html.create("color", (args, children) => {
+html.create("color", ({ args, content }) => {
 	return tag("span")
 		.attributes({ style: `color:${args[0]}` })
-		.body(children);
+		.body(content);
 });
 // Link
-html.create("link", (args, children) => {
-	return tag("a").attributes({ href: args[0], title: args[1] }).body(children);
+html.create("link", ({ args, content }) => {
+	return tag("a").attributes({ href: args[0], title: args[1] }).body(content);
 });
 // Image
-html.create("image", (args, children) => {
-	return tag("img").selfClose().attributes({ src: args[0], alt: children }).body("");
+html.create("image", ({ args, content }) => {
+	return tag("img").selfClose().attributes({ src: args[0], alt: content }).body("");
 });
 // Code
-html.create("code", (args, children) => {
-	const [content, cssTheme] = highlightCode(children, args[0].trim());
-	setHeader(null, { rawData: [cssTheme] });
+html.create("code", ({ args, content }) => {
+	const value = highlightCode(content, args[0].trim());
 	return tag("pre").body(
 		tag("code")
 			.attributes({ class: `hljs language-${args[0].trim()}` })
-			.body(content)
+			.body(value)
 	);
 });
 
+setHeader({ rawData: [cssTheme()] });
 export default html;
