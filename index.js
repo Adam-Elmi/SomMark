49ae4db7d17de99bfb58adf620d6d90448e393aa
Adam-Elmi <Adamcade123@gmail.com>
2025-12-26 16:10:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ import Mapper from "./mappers/mapper.js";
 import html from "./mappers/default_mode/smark.html.js";
 import markdown from "./mappers/default_mode/smark.md.js";
 import mdx from "./mappers/default_mode/smark.mdx.js";
+import { runtimeError } from "./core/validator.js";
 
 class SomMark {
-	constructor(src, format, targetFile = null, mode = "default") {
+	constructor({ src, format, targetFile = null, mode = "default", includeDocument = true }) {
 		this.src = src;
 		this.format = format;
 		this.targetFile = targetFile;
 		this.mode = mode;
 		this.Mapper = Mapper;
+		this.includeDocument = includeDocument;
+		const accepted_formats = ["html", "md", "mdx"];
+		if (!this.format) {
+			runtimeError(["{line}<$red:Undefined Format$>: <$yellow:Format argument is not defined.$>{line}"]);
+		}
+		if (this.format && !accepted_formats.includes(this.format)) {
+			runtimeError([
+				`{line}<$red:Unknown Format$>: <$yellow:You provided unknown format:$> <$green:'${format}'$>`,
+				`{N}<$yellow:Accepted formats are:$> [<$cyan: ${accepted_formats.join(", ")}$>]{line}`
+			]);
+		}
+		const formats = { html, md: markdown, mdx };
 		if (mode === "default" && this.format) {
-			const formats = { html, md: markdown, mdx };
 			this.targetFile = formats[this.format];
 		}
 	}
@@ -27,6 +39,7 @@ class SomMark {
 	}
 	transpile() {
 		const ast = this.parse();
-		return transpiler(ast, this.format, this.targetFile);
+		return transpiler(ast, this.format, this.targetFile, this.includeDocument);
 	}
 }
+export default SomMark;
