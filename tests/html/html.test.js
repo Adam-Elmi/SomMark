eb1b128c8e8255b143ab721d1580f8383bc4604e
Adam-Elmi <Adamcade123@gmail.com>
2025-12-28 00:17:00 +0300

    organized tests

--- tests/html/html.test.js
+++ tests/html/html.test.js
@@ -16,28 +16,23 @@ function removeNewline(text) {
 	}
 }
 
-describe("Transpiling -> [HTML] Test simple block", () => {
+html.create("Test", ({ args, content }) => {
+	return html.tag("div").attributes({ title: args[0], "data-test-id": args[1], "data-failed-tests": args[2] }).body(content);
+});
+describe("Transpiling -> [HTML]: Testing Blocks", () => {
 	it("returns <section><p>Test</p></section>", () => {
 		let output = new SomMark({ src: "[Section]\nTest\n[end]", format: "html", includeDocument: false }).transpile();
 		output = removeWhiteSpaces(output);
 		expect(output).toBe("<section><p>Test</p></section>");
 	});
-});
-
-describe("Transpiling -> [HTML] Testing if text is wrapped by paragraph tag", () => {
 	it("returns <p>Hello</p><p>World</p>", () => {
 		let output = new SomMark({ src: "[Block]\nHello\nWorld\n[end]", format: "html", includeDocument: false }).transpile();
 		output = removeWhiteSpaces(output);
 		expect(output).toBe("<p>Hello</p><p>World</p>");
 	});
-});
-
-html.create("Test", ({ args, content }) => {
-	return html.tag("div").attributes({ title: args[0], "data-test-id": args[1], "data-failed-tests": args[2] }).body(content);
-});
-describe("Transpiling -> [HTML] Testing Block arguments", () => {
-	const expectedValue = '<div title="Transpiler-Test" data-test-id="Test-101" data-failed-tests="5"><p>Block Arguments</p></div>';
 	it("It returns a block with its arguments", () => {
+		const expectedValue =
+			'<div title="Transpiler-Test" data-test-id="Test-101" data-failed-tests="5"><p>Block Arguments</p></div>';
 		let output = new SomMark({
 			src: "[Test = Transpiler-Test, Test-101, 5 ]\nBlock Arguments\n[end]",
 			format: "html",
@@ -47,27 +42,36 @@ describe("Transpiling -> [HTML] Testing Block arguments", () => {
 		expect(output).toBe(expectedValue);
 	});
 });
+
 /*
   Testig Inline Statements
 */
 describe("Transpiling -> [HTML] Testing Inline Statement", () => {
-	it("", () => {
-		let output = new SomMark({ src: "[Block]\n(SomMark)->(bold)[end]", format: "html", includeDocument: false }).transpile();
-		output = removeWhiteSpaces(output);
-		expect(output).toBe('<strong>SomMark</strong>');
+	it("returns strong tag", () => {
+		let inlineStatement_1 = new SomMark({
+			src: "[Block]\n(SomMark)->(bold)[end]",
+			format: "html",
+			includeDocument: false
+		}).transpile();
+		inlineStatement_1 = removeWhiteSpaces(inlineStatement_1);
+		expect(inlineStatement_1).toBe("<strong>SomMark</strong>");
 	});
-});
-describe("Transpiling -> [HTML] Testing Inline Statement", () => {
 	it("returns span tag with style attribute", () => {
-		let output = new SomMark({ src: "[Block]\n(SomMark)->(color:red)[end]", format: "html", includeDocument: false }).transpile();
-		output = removeNewline(output);
-		expect(output).toBe('<span style="color:red">SomMark</span>');
+		let inlineStatement_2 = new SomMark({
+			src: "[Block]\n(SomMark)->(color:red)[end]",
+			format: "html",
+			includeDocument: false
+		}).transpile();
+		inlineStatement_2 = removeNewline(inlineStatement_2);
+		expect(inlineStatement_2).toBe('<span style="color:red">SomMark</span>');
 	});
-});
-describe("Transpiling -> [HTML] Testing Inline Statement", () => {
-	it("returns span tag with style attribute", () => {
-		let output = new SomMark({ src: '[Block]\n(My Site)->(link:https://example.com "Title")[end]', format: "html", includeDocument: false }).transpile();
-		output = removeNewline(output);
-		expect(output).toBe('<a href="https://example.com" title="Title">My Site</a>');
+	it("returns anchor tag with href and title attributes", () => {
+		let inlineStatement_3 = new SomMark({
+			src: '[Block]\n(My Site)->(link:https://example.com "Title")[end]',
+			format: "html",
+			includeDocument: false
+		}).transpile();
+		inlineStatement_3 = removeNewline(inlineStatement_3);
+		expect(inlineStatement_3).toBe('<a href="https://example.com" title="Title">My Site</a>');
 	});
 });
