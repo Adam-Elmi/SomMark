196c196b55f7961e523a9ff7edc9645903e51ed4
Adam-Elmi <Adamcade123@gmail.com>
2025-12-27 23:46:47 +0300

    Update transpiler.test.js

--- tests/transpiler.test.js
+++ tests/transpiler.test.js
@@ -17,21 +17,3 @@ describe("SomMark format validation", () => {
 		}).toThrow(/Unknown Format/);
 	});
 });
-
-describe("Transpiler: Expected to be <p>Test</p>", () => {
-	it("returns <p>Test</p>", () => {
-		const output = new SomMark({ src, format: "html", includeDocument: false }).transpile().trim();
-		expect(output).toBe("<p>Test</p>");
-	});
-});
-
-describe("Transpiler: Expected to be <section><p>Test</p></section>", () => {
-	it("returns <section><p>Test</p></section>", () => {
-		const output = new SomMark({ src: "[Section]\nTest\n[end]", format: "html", includeDocument: false })
-			.transpile()
-			.split("")
-			.filter(value => !/\s/.test(value))
-			.join("");
-		expect(output).toBe("<section><p>Test</p></section>");
-	});
-});
