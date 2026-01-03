a2a90da1241d6734ee4819faa95040b15037c127
Adam-Elmi <Adamcade123@gmail.com>
2026-01-04 00:17:35 +0300

    Added sample file

---
+++ sample.js
@@ -0,0 +1,10 @@
+import fs from "node:fs/promises";
+import SomMark from "./index.js";
+
+const buffer = await fs.readFile("./example.smark");
+const file_content = buffer.toString();
+let smark = new SomMark({ src: file_content, format: "md", includeDocument: false });
+
+// console.log(JSON.stringify(smark.parse(), null, 2));
+// console.log(smark.lex());
+// console.log(smark.transpile());
