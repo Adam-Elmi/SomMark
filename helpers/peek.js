abcd6313b91f784ec95f77510b84fc277949df78
Adam-Elmi <Adamcade123@gmail.com>
2026-01-04 00:25:34 +0300

    Improved peek function

--- helpers/peek.js
+++ helpers/peek.js
@@ -1,4 +1,7 @@
 function peek(input, index, offset) {
+  if (input === null || index < 0 || offset < -index) {
+    return null;
+  }
   if (index + offset < input.length) {
     if (input[index + offset] !== undefined) {
       return input[index + offset];
