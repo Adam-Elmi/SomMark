1c359a650aaa217245f16be2fdc4d9906cb4c923
Adam-Elmi <Adamcade123@gmail.com>
2026-01-17 23:32:59 +0300

    Release v2.0.0-beta.1

--- custom.smark.js
+++ custom.smark.js
@@ -4,7 +4,7 @@ const custom_html = new Mapper();
 const { tag } = custom_html;
 
 custom_html.create("Hello", ({args, content}) => {
-	return tag("p").body(content);
+	return tag("Text").body(content);
 });
 
 export default custom_html;
