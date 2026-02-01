7ef97fb34ea9e0a240adc8e0c9ed3fda0f94b382
Adam-Elmi <Adamcade123@gmail.com>
2026-02-01 22:27:54 +0300

    Add CLI constants

---
+++ cli/constants.js
@@ -0,0 +1,12 @@
+// ========================================================================== //
+//  CLI Constants                                                             //
+// ========================================================================== //
+
+export const options = ["-v", "--version", "-h", "--help", "--html", "--markdown", "--mdx", "--text", "--print", "-p"];
+
+export const extensions = {
+    text: "txt",
+    html: "html",
+    markdown: "md",
+    mdx: "mdx"
+};
