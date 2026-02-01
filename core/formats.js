5c655741f88f6e4525bc5b62b177fd98441f6c7d
Adam-Elmi <Adamcade123@gmail.com>
2026-02-01 22:27:54 +0300

    Add core/formats.js

---
+++ core/formats.js
@@ -0,0 +1,8 @@
+const formats = {
+  textFormat: "text",
+	htmlFormat: "html",
+	markdownFormat: "markdown",
+	mdxFormat: "mdx"
+};
+
+export const {textFormat, htmlFormat, markdownFormat, mdxFormat} = formats;
