85d810efd5f9afa7e0420c1306cc4707994d5de3
Adam-Elmi <Adamcade123@gmail.com>
2026-02-01 22:27:54 +0300

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,26 +1,11 @@
 import fs from "node:fs/promises";
 import SomMark from "./index.js";
+import custom_html from "./custom.smark.js";
 
 const buffer = await fs.readFile("./example.smark");
 const file_content = buffer.toString();
-let smark = new SomMark({ src: file_content, format: "md", includeDocument: false });
+let smark = new SomMark({ src: file_content, format: "html", includeDocument: false });
 
 // console.log(JSON.stringify(smark.parse(), null, 2));
 // console.log(smark.lex());
-console.log(smark.transpile());
-
-
-// Input: "title:SomMark, description:Docs"
-
-// const args = [
-//   "SomMark",  // Index 0 (Raw value)
-//   "Docs"      // Index 1 (Raw value)
-// ];
-
-// // The Parser ALSO attaches the keys map to the same object:
-// args["title"] = "SomMark";
-// args["description"] = "Docs";
-
-// // Now both work:
-// console.log(Object.entries(args));       // "SomMark"
-// console.log(args["title"]); // "SomMark"
+console.log(await smark.transpile());
