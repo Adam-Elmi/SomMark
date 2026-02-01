2108db1e5a4ea060e559f7912025c79d8628b35a
Adam-Elmi <Adamcade123@gmail.com>
2026-02-01 22:27:54 +0300

    Add CLI help command

---
+++ cli/commands/help.js
@@ -0,0 +1,43 @@
+import { formatMessage } from "../../core/errors.js";
+import { options } from "../constants.js";
+
+// ========================================================================== //
+//  Help Command                                                              //
+// ========================================================================== //
+
+export function getHelp(unknown_option = true) {
+    const msg = [
+        `${unknown_option && process.argv[2] ? `<$red:Unrecognized option$> <$blue: '${process.argv[2]}'$>` : ""}`,
+        "{N}<$yellow:Usage:$> <$blue:smark [option]$>",
+
+        "{N}{N}<$yellow:Global Options:$>",
+        "{N}  <$green:-h, --help$>     <$cyan: Show help message$>",
+        "{N}  <$green:-v, --version$>  <$cyan: Show version information$>",
+
+        "{N}{N}<$yellow:Transpilation Options:$>",
+        "{N}<$yellow:Usage:$> <$blue:smark [option] [targetFile] [option] [outputFile] [outputDir]$>",
+        "{N}  <$green:--html$>         <$cyan: Transpile to HTML$>",
+        "{N}  <$green:--markdown$>     <$cyan: Transpile to Markdown$>",
+        "{N}  <$green:--mdx$>          <$cyan: Transpile to MDX$>",
+        "{N}  <$green:--text$>         <$cyan: Transpile to plain text$>",
+
+        "{N}{N}<$yellow:Output Options:$>",
+        "{N}  <$green:-p, --print$>    <$cyan: Print output to console (stdout)$>",
+        "{N}  <$green:-o$>             <$cyan: Specify output filename (and optionally directory)$>",
+
+        "{N}{N}<$yellow:Examples:$>",
+        "{N}  <$magenta:1. Basic usage:$> <$blue:smark --html input.smark$>",
+        "{N}  <$magenta:2. Print to console:$> <$blue:smark --html -p input.smark$>",
+        "{N}  <$magenta:3. Custom output:$> <$blue:smark --html input.smark -o myOutput ./dist/$>"
+    ].join("");
+    const help_msg = formatMessage(msg);
+
+    if (!options.includes(process.argv[2]) && unknown_option) {
+        console.log(help_msg);
+        process.exit(0);
+    } else if (process.argv[2] === "-h" || process.argv[2] === "--help") {
+        console.log(help_msg);
+        process.exit(0);
+    }
+    return help_msg;
+}
