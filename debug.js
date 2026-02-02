711e2bd711be7778b03a4cd76cf9a02fac4b2891
Adam-Elmi <Adamcade123@gmail.com>
2026-02-03 00:13:01 +0300

    Updated debug script

--- debug.js
+++ debug.js
@@ -1,10 +1,9 @@
 import fs from "node:fs/promises";
 import SomMark from "./index.js";
-import custom_html from "./custom.smark.js";
 
 const buffer = await fs.readFile("./example.smark");
 const file_content = buffer.toString();
-let smark = new SomMark({ src: file_content, format: "html", includeDocument: false });
+let smark = new SomMark({ src: file_content, format: "html", includeDocument: true });
 
 // console.log(JSON.stringify(smark.parse(), null, 2));
 // console.log(smark.lex());
