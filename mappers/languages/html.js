c4c15b7a0b4cb5e001b75e311d206f699fc1ddb0
Adam-Elmi <Adamcade123@gmail.com>
2026-02-20 23:08:23 +0300

    Update html.js

--- mappers/languages/html.js
+++ mappers/languages/html.js
@@ -1,16 +1,21 @@
 import Mapper from "../mapper.js";
 const HTML = new Mapper();
-const { tag, code, list } = HTML;
+const { tag, code, list, safeArg } = HTML;
 
 HTML.register(["Html", "html"], ({ args }) => {
-	if (args) {
-		HTML.pageProps.pageTitle = args.title ?? args.title ? args.title : HTML.pageProps.pageTitle;
-		HTML.pageProps.charset = args.charset ? args.charset : HTML.pageProps.charset;
-		HTML.pageProps.tabIcon.src = args.iconSrc ? args.iconSrc : HTML.pageProps.tabIcon.src;
-		HTML.pageProps.tabIcon.type = args.iconType ? args.iconType : HTML.pageProps.tabIcon.type;
-		HTML.pageProps.httpEquiv["X-UA-Compatible"] = args.httpEquiv ? args.httpEquiv : HTML.pageProps.httpEquiv["X-UA-Compatible"];
-		HTML.pageProps.viewport = args.viewport ? args.viewport : HTML.pageProps.viewport;
-	}
+	HTML.pageProps.pageTitle = safeArg(args, undefined, "title", null, null, HTML.pageProps.pageTitle);
+	HTML.pageProps.charset = safeArg(args, undefined, "charset", null, null, HTML.pageProps.charset);
+	HTML.pageProps.tabIcon.src = safeArg(args, undefined, "iconSrc", null, null, HTML.pageProps.tabIcon.src);
+	HTML.pageProps.tabIcon.type = safeArg(args, undefined, "iconType", null, null, HTML.pageProps.tabIcon.type);
+	HTML.pageProps.httpEquiv["X-UA-Compatible"] = safeArg(
+		args,
+		undefined,
+		"httpEquiv",
+		null,
+		null,
+		HTML.pageProps.httpEquiv["X-UA-Compatible"]
+	);
+	HTML.pageProps.viewport = safeArg(args, undefined, "viewport", null, null, HTML.pageProps.viewport);
 	return "";
 });
 
@@ -44,35 +49,35 @@ HTML.register(["bold", "b"], ({ content }) => {
 HTML.register(["italic", "i"], ({ content }) => {
 	return tag("i").body(content);
 });
-// Italic
+// Emphasis
 HTML.register(["emphasis", "e"], ({ content }) => {
 	return tag("span").attributes({ style: "font-weight:bold; font-style: italic;" }).body(content);
 });
 // Colored Text
 HTML.register("color", ({ args, content }) => {
+	const color = safeArg(args, 0, undefined, null, null, "none");
 	return tag("span")
-		.attributes({ style: `color:${args[0]}` })
+		.attributes({ style: `color:${color}` })
 		.body(content);
 });
 // Link
 HTML.register("link", ({ args, content }) => {
-	return tag("a")
-		.attributes({ href: args[0].trim(), title: args[1] ? args[1].trim() : "" })
-		.body(content);
+	const url = safeArg(args, 0, "url", null, null, "");
+	const title = safeArg(args, 1, "title", null, null, "");
+	return tag("a").attributes({ href: url.trim(), title: title.trim() }).body(content);
 });
 // Image
 HTML.register(
 	["image", "Image"],
 	({ args }) => {
-		const src = args && args["src"] ? args["src"] : "";
-		const alt = args && args["alt"] ? args["alt"] : "";
-		const width = args && args["width"] ? args["width"] : "",
-			height = args && args["height"] ? args["height"] : "";
+		const src = safeArg(args, undefined, "src", null, null, "");
+		const alt = safeArg(args, undefined, "alt", null, null, "");
+		const width = safeArg(args, undefined, "width", null, null, "");
+		const height = safeArg(args, undefined, "height", null, null, "");
 		return tag("img").attributes({ src, alt, width, height }).selfClose();
 	},
 	{
 		rules: {
-			is_Self_closing: true,
 			args: {
 				required: ["src"]
 			}
@@ -118,9 +123,8 @@ HTML.register(
 // Todo
 HTML.register("todo", ({ args, content }) => {
 	const checked = HTML.todo(content);
-	return tag("div").body(
-		tag("input").attributes({ type: "checkbox", disabled: true, checked }).selfClose() + (args[0] ? args[0] : "")
-	);
+	const task = safeArg(args, 0, undefined, null, null, "");
+	return tag("div").body(tag("input").attributes({ type: "checkbox", disabled: true, checked }).selfClose() + task);
 });
 
 export default HTML;
