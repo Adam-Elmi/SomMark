6df248915288a1d06a94826cddc0dd22287ffa9b
Adam-Elmi <Adamcade123@gmail.com>
2026-02-21 20:51:18 +0300

    Update config.js

--- cli/helpers/config.js
+++ cli/helpers/config.js
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { pathToFileURL } from "node:url";
 import { isExist } from "./file.js";
+import { getConfigDir } from "../commands/init.js";
 
 // ========================================================================== //
 //  Configuration Loader                                                      //
@@ -8,7 +9,7 @@ import { isExist } from "./file.js";
 
 const CONFIG_FILE_NAME = "smark.config.js";
 const currentDir = process.cwd();
-const configPath = path.join(currentDir, CONFIG_FILE_NAME);
+const localConfigPath = path.join(currentDir, CONFIG_FILE_NAME);
 
 // ========================================================================== //
 //  Default Configuration                                                     //
@@ -16,7 +17,6 @@ const configPath = path.join(currentDir, CONFIG_FILE_NAME);
 let config = {
     outputFile: "output",
     outputDir: "",
-    mode: "default",
     mappingFile: ""
 };
 
@@ -24,13 +24,22 @@ let config = {
 //  Load Configuration                                                        //
 // ========================================================================== //
 export async function loadConfig() {
-    if (await isExist(configPath)) {
+    const userConfigPath = path.join(getConfigDir(), CONFIG_FILE_NAME);
+    let targetConfigPath = null;
+
+    if (await isExist(userConfigPath)) {
+        targetConfigPath = userConfigPath;
+    } else if (await isExist(localConfigPath)) {
+        targetConfigPath = localConfigPath;
+    }
+
+    if (targetConfigPath) {
         try {
-            const configURL = pathToFileURL(configPath).href;
+            const configURL = pathToFileURL(targetConfigPath).href;
             const loadedModule = await import(configURL);
             config = loadedModule.default || loadedModule;
         } catch (error) {
-            console.error(`Error loading configuration file ${CONFIG_FILE_NAME}:`, error.message);
+            console.error(`Error loading configuration file ${targetConfigPath}:`, error.message);
         }
     } else {
         // console.log(`${CONFIG_FILE_NAME} not found. Using default configuration.`);
