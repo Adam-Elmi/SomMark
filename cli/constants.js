1a3c202e7196179eed26a53128785a4eec6b5d1b
Adam-Elmi <Adamcade123@gmail.com>
2026-02-23 08:46:49 +0300

    Update constants.js

--- cli/constants.js
+++ cli/constants.js
@@ -2,11 +2,12 @@
 //  CLI Constants                                                             //
 // ========================================================================== //
 
-export const options = ["-v", "--version", "-h", "--help", "--html", "--markdown", "--mdx", "--text", "--print", "-p"];
+export const options = ["-v", "--version", "-h", "--help", "--html", "--markdown", "--mdx", "json", "--text", "--print", "-p"];
 
 export const extensions = {
-    text: "txt",
-    html: "html",
-    markdown: "md",
-    mdx: "mdx"
+	text: "txt",
+	html: "html",
+	markdown: "md",
+	mdx: "mdx",
+	json: "json"
 };
