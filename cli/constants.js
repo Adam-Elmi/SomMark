26926abf91a19af9682d9d87f0ea691a3ebd755e
Adam-Elmi <Adamcade123@gmail.com>
2026-03-07 09:28:10 +0300

    Fixed: CLI Fails to Print JSON Output with --json Flag #8

--- cli/constants.js
+++ cli/constants.js
@@ -2,7 +2,7 @@
 //  CLI Constants                                                             //
 // ========================================================================== //
 
-export const options = ["-v", "--version", "-h", "--help", "--html", "--markdown", "--mdx", "json", "--text", "--print", "-p"];
+export const options = ["-v", "--version", "-h", "--help", "--html", "--markdown", "--mdx", "--json", "--text", "--print", "-p"];
 
 export const extensions = {
 	text: "txt",
