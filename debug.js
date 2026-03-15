f7dbd68ace2ddcaba6b13a544da210a26a061c17
Adam-Elmi <Adamcade123@gmail.com>
2026-03-16 02:51:09 +0300

    Update debug.js: updated debug.js for v3 release

--- debug.js
+++ debug.js
@@ -1,15 +1,15 @@
 import fs from "node:fs/promises";
 import SomMark from "./index.js";
 
-const buffer = await fs.readFile("./example.smark");
+const buffer = await fs.readFile("./examples/markdown/tasks.smark");
 const file_content = buffer.toString();
 let smark = new SomMark({ 
     src: file_content, 
-    format: "html", 
-    includeDocument: false,
-    plugins: [{name: "raw-content", options: {targetBlocks: ["Block", "div", "Div"]}}]
+    format: "markdown", 
+    includeDocument: true,
 });
 
-console.log(JSON.stringify(await smark.parse(), null, 2));
+
+// console.log(JSON.stringify(await smark.parse(), null, 2));
 // console.log(await smark.lex());
-// console.log(await smark.transpile());
+console.log(await smark.transpile());
