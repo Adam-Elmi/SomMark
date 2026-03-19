7ab067540e2a71eb9da60313f760f3cde70e5938
Adam-Elmi <Adamcade123@gmail.com>
2026-03-20 02:36:31 +0300

    Update utility functions

--- helpers/utils.js
+++ helpers/utils.js
@@ -63,6 +63,33 @@ export function safeArg(args, index, key, type = null, setType = null, fallBack 
 }
 
 /**
+ * Calculates the Levenshtein distance between two strings.
+ * @param {string} a 
+ * @param {string} b 
+ * @returns {number}
+ */
+export function levenshtein(a, b) {
+	const matrix = [];
+	for (let i = 0; i <= b.length; i++) matrix[i] = [i];
+	for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
+
+	for (let i = 1; i <= b.length; i++) {
+		for (let j = 1; j <= a.length; j++) {
+			if (b.charAt(i - 1) === a.charAt(j - 1)) {
+				matrix[i][j] = matrix[i - 1][j - 1];
+			} else {
+				matrix[i][j] = Math.min(
+					matrix[i - 1][j - 1] + 1,
+					matrix[i][j - 1] + 1,
+					matrix[i - 1][j] + 1
+				);
+			}
+		}
+	}
+	return matrix[b.length][a.length];
+}
+
+/**
  * Renders an HTML table.
  */
 export function htmlTable(data, headers, escapeFn = (t) => t) {
