d1a5f59e0b6bf4cdabda30257aa291f5ad9ffc56
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:43 +0300

    Updated cli logic in help.js

--- cli/commands/help.js
+++ cli/commands/help.js
@@ -5,6 +5,11 @@ import { options } from "../constants.js";
 //  Help Command                                                              //
 // ========================================================================== //
 
+/**
+ * Displays the CLI help message.
+ * @param {boolean} [unknown_option=true] - Whether to show the "Unrecognized option" prefix if an invalid flag is passed.
+ * @returns {string} - The formatted help message.
+ */
 export function getHelp(unknown_option = true) {
 	const msg = [
 		`${unknown_option && process.argv[2] ? `<$red:Unrecognized option$> <$blue: '${process.argv[2]}'$>` : ""}`,
@@ -13,32 +18,27 @@ export function getHelp(unknown_option = true) {
 		"{N}{N}<$yellow:Global Options:$>",
 		"{N}  <$green:-h, --help$>     <$cyan: Show help message$>",
 		"{N}  <$green:-v, --version$>  <$cyan: Show version information$>",
-		"{N}  <$green:init$>           <$cyan: Initialize global SomMark configuration directory$>",
+		"{N}  <$green:init$>           <$cyan: Initialize local SomMark configuration file (smark.config.js)$>",
 		"{N}  <$green:show config$>    <$cyan: Display the absolute paths to the active SomMark configuration files$>",
-		"{N}  <$green:color on|off$>   <$cyan: Enable or disable colored output$>",
-		"{N}  <$green:list plugins$>    <$cyan: List all enabled plugins with author and description$>",
-		"{N}  <$green:list -i plugins$> <$cyan: List only internal (built-in) plugins$>",
-		"{N}  <$green:list -e plugins$> <$cyan: List only external (user-defined) plugins$>",
-		"{N}  <$green:list pipeline$>   <$cyan: Show the execution order of all active plugin phases$>",
+		"{N}  <$green:color on|off$>   <$cyan: Help on enabling colors via Environment Variables$>",
 
 		"{N}{N}<$yellow:Transpilation Options:$>",
 		"{N}<$yellow:Usage:$> <$blue:sommark [option] [targetFile] [option] [outputFile] [outputDir]$>",
 		"{N}  <$green:--html$>         <$cyan: Transpile to HTML$>",
 		"{N}  <$green:--markdown$>     <$cyan: Transpile to Markdown$>",
 		"{N}  <$green:--mdx$>          <$cyan: Transpile to MDX$>",
-		"{N}  <$green:--json$>         <$cyan: Transpile to json$>",
+		"{N}  <$green:--json$>         <$cyan: Transpile to JSON$>",
 		"{N}  <$green:--text$>         <$cyan: Transpile to plain text$>",
 		"{N}  <$green:--lex$>          <$cyan: Print lexer tokens to console$>",
 		"{N}  <$green:--parse$>        <$cyan: Print parser AST to console$>",
 
-		"{N}{N}<$yellow:Output Options:$>",
+		"{N}{N}<$yellow:Output & Config Overrides:$>",
 		"{N}  <$green:-p, --print$>    <$cyan: Print output to console (stdout)$>",
 		"{N}  <$green:-o$>             <$cyan: Specify output filename (and optionally directory)$>",
 
 		"{N}{N}<$yellow:Examples:$>",
 		"{N}  <$magenta:1. Basic usage:$> <$blue:sommark --html input.smark$>",
-		"{N}  <$magenta:2. Print to console:$> <$blue:sommark --html -p input.smark$>",
-		"{N}  <$magenta:3. Custom output:$> <$blue:sommark --html input.smark -o myOutput ./dist/$>"
+		"{N}  <$magenta:2. Custom output:$> <$blue:sommark --html input.smark -o myOutput ./dist/$>"
 	].join("");
 	const help_msg = formatMessage(msg);
 
