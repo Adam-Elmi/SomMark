2d95edde3aabc1560963c36f44a3f92d1253a283
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:44 +0300

    Updated core logic in tokenTypes.js

--- core/tokenTypes.js
+++ core/tokenTypes.js
@@ -1,11 +1,44 @@
-// Token Types in SomMark
+/**
+ * Token Types in SomMark.
+ * These represent the basic lexical atoms identified by the lexer.
+ * 
+ * @constant {Object}
+ * @property {string} OPEN_BRACKET - '[' char.
+ * @property {string} CLOSE_BRACKET - ']' char.
+ * @property {string} END_KEYWORD - 'end' value.
+ * @property {string} IDENTIFIER - Block or inline name (e.g. 'Person', 'import', '$use-module').
+ * @property {string} EQUAL - '=' char.
+ * @property {string} VALUE - Data values. Encapsulates Quoted Strings ("...") and Prefix Layers (js{}, p{}).
+ * @property {string} TEXT - Plain unformatted text content.
+ * @property {string} THIN_ARROW - '->' sequence.
+ * @property {string} OPEN_PAREN - '(' char.
+ * @property {string} CLOSE_PAREN - ')' char.
+ * @property {string} OPEN_AT - '@_' sequence (At-Block start).
+ * @property {string} CLOSE_AT - '_@' sequence (At-Header end).
+ * @property {string} COLON - ':' char.
+ * @property {string} COMMA - ',' char.
+ * @property {string} SEMICOLON - ';' char (At-Block separator).
+ * @property {string} COMMENT - '#' comments.
+ * @property {string} ESCAPE - '\' char. Used for literalizing structural chars like '\"' or '\['.
+ * @property {string} QUOTE - '"' delimiter.
+ * @property {string} IMPORT - 'import' keyword.
+ * @property {string} USE_MODULE - '$use-module' keyword.
+ * @property {string} PREFIX_JS - 'js{}' prefix layer.
+ * @property {string} PREFIX_P - 'p{}' placeholder layer.
+ * @property {string} EOF - End of File indicator.
+ */
 const TOKEN_TYPES = {
   OPEN_BRACKET: "OPEN_BRACKET",
   CLOSE_BRACKET: "CLOSE_BRACKET",
   END_KEYWORD: "END_KEYWORD",
+  IMPORT: "IMPORT",
+  USE_MODULE: "USE_MODULE",
   IDENTIFIER: "IDENTIFIER",
   EQUAL: "EQUAL",
   VALUE: "VALUE",
+  QUOTE: "QUOTE",
+  PREFIX_JS: "PREFIX_JS",
+  PREFIX_P: "PREFIX_P",
   TEXT: "TEXT",
   THIN_ARROW: "THIN_ARROW",
   OPEN_PAREN: "OPEN_PAREN",
@@ -17,6 +50,8 @@ const TOKEN_TYPES = {
   SEMICOLON: "SEMICOLON",
   COMMENT: "COMMENT",
   ESCAPE: "ESCAPE",
+  KEY: "KEY",
+  WHITESPACE: "WHITESPACE",
   EOF: "EOF"
 };
 
