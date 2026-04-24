5bf030b41c9308ee08914871613b2d79627f859a
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:44 +0300

    Updated cli logic in version.js

--- cli/commands/version.js
+++ cli/commands/version.js
@@ -4,12 +4,18 @@ import projectJson from "../../package.json" with { type: "json" };
 //  Version Command                                                           //
 // ========================================================================== //
 
+/**
+ * Prints the current version of SomMark (from package.json) to the console.
+ */
 export function printVersion() {
     if (projectJson && projectJson.version) {
         console.log(projectJson.version);
     }
 }
 
+/**
+ * Prints the SomMark header/banner with version, description, and copyright information.
+ */
 export function printHeader() {
     console.log(
         [
