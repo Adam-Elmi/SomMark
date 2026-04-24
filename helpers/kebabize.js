b4a0fadcb6976e23b40cd7594d7dec8c112347af
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:46 +0300

    Updated utility: kebabize.js

--- helpers/kebabize.js
+++ helpers/kebabize.js
@@ -1,2 +1,8 @@
+/**
+ * Changes CamelCase or PascalCase names into kebab-case.
+ * 
+ * @param {string} str - The string to convert.
+ * @returns {string} - The kebab-cased string.
+ */
 const kebabize = str => str.replace(/[A-Z]/g, m => `-${m.toLowerCase()}`);
 export default kebabize;
