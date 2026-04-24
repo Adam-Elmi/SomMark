b26b52f3f5025d7c0d5e8def8f00711a575ffee1
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:46 +0300

    Updated utility: escapeHTML.js

--- helpers/escapeHTML.js
+++ helpers/escapeHTML.js
@@ -1,8 +1,15 @@
+/**
+ * Makes a string safe to display in HTML by replacing special characters.
+ * This helps prevent security issues and formatting errors.
+ * 
+ * @param {string} str - The raw string to escape.
+ * @returns {string} - The HTML-safe string.
+ */
 export default function escapeHTML(str) {
-	return String(str)
-		.replace(/&/g, "&amp;")
-		.replace(/</g, "&lt;")
-		.replace(/>/g, "&gt;")
-		.replace(/"/g, "&quot;")
-		.replace(/'/g, "&#39;");
+        return String(str)
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#39;");
 }
