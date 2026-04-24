1b5f71618a5e606c8c4f5f4e7d55e0feacd9697e
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:44 +0300

    Updated cli logic in config.js

--- cli/helpers/config.js
+++ cli/helpers/config.js
@@ -6,12 +6,23 @@ import { findAndLoadConfig } from "../../core/helpers/config-loader.js";
 
 let resolvedConfigPath = null;
 
+/**
+ * Loads the SomMark settings (configuration) for a specific file or folder.
+ * Saves the path to the settings file for later.
+ * 
+ * @param {string|null} [filename=null] - The file path to start searching for a config from.
+ * @returns {Promise<Object>} - The loaded configuration object.
+ */
 export async function loadConfig(filename = null) {
 	const config = await findAndLoadConfig(filename);
 	resolvedConfigPath = config.resolvedConfigPath;
 	return config;
 }
 
+/**
+ * Returns the absolute path to the configuration file that was last loaded.
+ * @returns {string|null}
+ */
 export function getResolvedConfigPath() {
 	return resolvedConfigPath;
 }
