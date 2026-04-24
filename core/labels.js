1332624b3665ff33595cc73b28eb1c0a77941f04
Adam-Elmi <Adamcade123@gmail.com>
2026-04-24 23:18:44 +0300

    Updated core logic in labels.js

--- core/labels.js
+++ core/labels.js
@@ -1,27 +1,39 @@
+/**
+ * These labels identify different parts of the code (like blocks or text) 
+ * so the system knows how to handle them.
+ */
 export const BLOCK = "Block",
 	TEXT = "Text",
 	INLINE = "Inline",
 	ATBLOCK = "AtBlock",
 	COMMENT = "Comment",
 	IMPORT = "Import",
-	USE_MODULE = "$use-module",
-	SEMICOLON = "Semicolon",
+	USE_MODULE = "$use-module";
+
+/**
+ * Names for symbols used to separate parts of the code (like commas and colons).
+ */
+export const SEMICOLON = "Semicolon",
 	BLOCKCOMMA = "Block-comma",
 	ATBLOCKCOMMA = "Atblock-comma",
 	INLINECOMMA = "Inline-comma",
 	BLOCKCOLON = "Block-colon",
 	ATBLOCKCOLON = "Atblock-colon",
-	INLINECOLON = "Inline-colon",
-	block_id = "Block Identifier",
+	INLINECOLON = "Inline-colon";
+
+/**
+ * These names are used in error messages to tell you exactly which part 
+ * of your code has a mistake.
+ */
+export const block_id = "Block Identifier",
 	block_value = "Block Value",
-	block_id_2 = "Block Identifier 2",
+	block_key = "Block Key",
 	block_end = "Block end",
 	inline_id = "Inline Identifier",
-	inline_value = "Inline Value",
-	inline_id_2 = "Inline Identifier 2",
+	inline_text = "Inline Text",
 	at_id = "At Identifier",
 	at_value = "At Value",
-	at_id_2 = "At Identifier 2",
+	atblock_key = "AtBlock Key",
 	at_end = "Atblock End",
-	block_end_2 = "Block End 2",
+	/** Reserved keyword for closing blocks */
 	end_keyword = "end";
