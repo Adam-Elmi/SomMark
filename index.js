15cb37fddb68c2da1bf43c0128238848bcc23493
Adam-Elmi <Adamcade123@gmail.com>
2026-06-09 19:31:42 +0000

    feat: add async exists/readFile wrappers to Node.js fs and call setDefaultCwd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import SomMark, { setDefaultFs, setDefaultFindAndLoadConfig } from "./index.shared.js";
+import SomMark, { setDefaultFs, setDefaultCwd, setDefaultFindAndLoadConfig } from "./index.shared.js";
 export * from "./index.shared.js";
 
 // Node-specific filesystem import
@@ -6,9 +6,13 @@ let nodeFs = null;
 if (typeof process !== "undefined" && process.versions?.node) {
 	try {
 		nodeFs = await import("node:fs").then(m => m.default || m);
+		// Add async interface so modules.js can use await fs.exists / await fs.readFile
+		nodeFs.exists = (p) => nodeFs.promises.access(p).then(() => true).catch(() => false);
+		nodeFs.readFile = (p, enc) => nodeFs.promises.readFile(p, enc);
 	} catch (e) {}
 }
 setDefaultFs(nodeFs);
+if (typeof process !== "undefined" && process.cwd) setDefaultCwd(process.cwd());
 
 // Node-specific config-loader import
 let findAndLoadConfigFn = async () => ({});
