30c7b233737d5e0918d55c7f2b6c219c7b3a8ceb
Adam-Elmi <Adamcade123@gmail.com>
2026-06-23 04:41:09 +0300

    fix: fallbackTarget default changed from 'style' to true in defaultConfig

--- core/helpers/config-loader.js
+++ core/helpers/config-loader.js
@@ -83,7 +83,7 @@ export async function findAndLoadConfig(targetPath) {
 		placeholders: {},
 		customProps: [],
 		importAliases: {},
-		fallbackTarget: "style",
+		fallbackTarget: true,
 		outputValidator: null,
 		baseDir: null,
 		showSpinner: true,
