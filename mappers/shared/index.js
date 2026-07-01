5ce71d9ac4ea4901d45b94fb4dc890722c3bcb38
Adam-Elmi <Adamcade123@gmail.com>
2026-07-01 07:36:38 +0300

    Add shared block across all SomMark mappers

--- mappers/shared/index.js
+++ mappers/shared/index.js
@@ -1,8 +1,17 @@
 /**
  * Registers universal utility blocks shared across all SomMark mappers.
- * These blocks are considered "Format Agnostic."
  *
  * @param {Mapper} mapper - The mapper instance to register tags on.
  */
 export function registerSharedOutputs(mapper) {
+	mapper.register(
+		["raw", "Raw"],
+		({ content }) => {
+			return content;
+		},
+    {
+      escape: false, rules: {
+      required_args: ["smark-raw"]
+		} }
+	);
 }
